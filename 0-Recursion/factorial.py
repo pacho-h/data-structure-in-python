8a8d392b13c9b3cdc4cbd29a7230035eeee29be6
pacho-h <pacho.h90@gmail.com>
2023-12-26 10:15:51 +0900

    add factorial example

--- 0-Recursion/factorial.py
+++ 0-Recursion/factorial.py
@@ -1,2 +1,16 @@
-function main():
-    print("Recursion practice - factorial")
+def factorial(n, expression):
+    if n <= 1:
+        return (1, f"{expression}{n}")
+    else:
+        expression += f"{n} * "
+        child, child_expression = factorial(n-1, expression)
+        result = n * child
+        return (result, child_expression)
+
+print("Recursion practice - factorial\nEnter a number")
+input_number = int(input())
+
+(result, expression) = factorial(input_number, "")
+print(f"{input_number}! = {expression}")
+print(f" = {result}")
+
